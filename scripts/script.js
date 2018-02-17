16ce6937f280df932c40860e926ec719e9796e1b
Hikari <hyamasaki0101@gmail.com>
2018-02-17 18:06:50 +0900

    スクロールアニメーションを追加。

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
 $(function(){
   // btn-to-topを押したら一番上にもどる
   $('.btn-to-top').click(function(){
-    $('html, body').scrollTop(0);
+    $('html, body').animate({'scrollTop':0},500);
   });
+
+// topのFoodsのカードを押すと、Foodsのセクションにスクロール
+  $('.top-image-food').click(function(){
+    $('html, body').animate({'scrollTop':550},500);
+    });
+
+// topのNatureのカードを押すと、Natureのセクションにスクロール
+  $('.top-image-nature').click(function(){
+    $('html, body').animate({'scrollTop':1300},600);
+    });
+
+// topのMapのカードを押すと、Accessのセクションにスクロール
+  $('.top-image-map').click(function(){
+    $('html, body').animate({'scrollTop':2000},700);
+    });
+
 
 });
