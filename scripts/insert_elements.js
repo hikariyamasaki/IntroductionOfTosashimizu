436e7b41cbc61e8c10bc658a98b53d1a3ed36b90
Hikari <hyamasaki0101@gmail.com>
2018-02-17 19:40:42 +0900

    フォント追加。 jsファイル追加。

---
+++ scripts/insert_elements.js
@@ -0,0 +1,42 @@
+function insertElements(titleList,imageList,divName){
+  let divElementRight = document.getElementById(divName)
+  let stockElementDetail = ''
+  for (var i in titleList) {
+    stockElementDetail += '<div class="element-detail">  <img src="'+ imageList[i] +'" class="element-image"/>  <p>    '+ titleList[i] +'  </p></div>'
+  }
+
+  divElementRight.innerHTML = stockElementDetail
+}
+
+
+
+{
+
+  let divFoods = 'food-element'
+  let foodTitles = ['長太郎貝','ペラ焼き','清水鯖','くじら料理','ウツボ料理','刺身']
+  let foodImages = [
+    './image/foods-element.jpg',
+    './image/foods-element.jpg',
+    './image/foods-element.jpg',
+    './image/foods-element.jpg',
+    './image/foods-element.jpg',
+    './image/foods-element.jpg',
+  ]
+
+  insertElements(foodTitles,foodImages,divFoods)
+
+  let divNature = 'nature-element'
+  let natureTitles = ['唐人駄馬遺跡','とまろっと','竜串','足摺岬','大岐ビーチ','白山洞門']
+  let natureImages = [
+    './image/nature-element.png',
+    './image/nature-element.png',
+    './image/nature-element.png',
+    './image/nature-element.png',
+    './image/nature-element.png',
+    './image/nature-element.png',
+  ]
+
+  insertElements(natureTitles,natureImages,divNature)
+
+
+}
