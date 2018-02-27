141af5702249c7578c9671f501e20a9e94bb0d37
Hikari <hyamasaki0101@gmail.com>
2018-02-27 20:08:13 +0900

    画像編集

--- scripts/insert_elements.js
+++ scripts/insert_elements.js
@@ -16,11 +16,11 @@ function insertElements(titleList,imageList,divName){
   let foodTitles = ['長太郎貝','ペラ焼き','清水鯖','くじら料理','ウツボ料理','刺身']
   let foodImages = [
     './image/foods-element.jpg',
-    './image/foods-element.jpg',
-    './image/foods-element.jpg',
-    './image/foods-element.jpg',
-    './image/foods-element.jpg',
-    './image/foods-element.jpg',
+    './image/perayaki.png',
+    './image/saba.png',
+    './image/kujira.png',
+    './image/utsubo.png',
+    './image/sashimi.jpg',
   ]
 
   insertElements(foodTitles,foodImages,divFoods)
@@ -29,11 +29,11 @@ function insertElements(titleList,imageList,divName){
   let natureTitles = ['唐人駄馬遺跡','とまろっと','竜串','足摺岬','大岐ビーチ','白山洞門']
   let natureImages = [
     './image/nature-element.png',
-    './image/nature-element.png',
-    './image/nature-element.png',
-    './image/nature-element.png',
-    './image/nature-element.png',
-    './image/nature-element.png',
+    './image/tomarotto.png',
+    './image/tatsukusi.png',
+    './image/ashizurimisaki.png',
+    './image/oki.png',
+    './image/sirayamadosan.png',
   ]
 
   insertElements(natureTitles,natureImages,divNature)
